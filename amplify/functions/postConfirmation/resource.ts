9bcd6e8f0509dacb389be667cb72f81f89771ec5
Akshat Shah <akshat.s@shaip.com>
2026-03-06 14:16:45 +0530

    fix changes

--- amplify/functions/postConfirmation/resource.ts
+++ amplify/functions/postConfirmation/resource.ts
@@ -3,5 +3,4 @@ import { defineFunction } from '@aws-amplify/backend';
 export const postConfirmation = defineFunction({
   name: 'postConfirmation',
   entry: './handler.ts',
-  resourceGroupName: 'auth',
 });
